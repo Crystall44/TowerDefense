88d2d8e4cf9f382876babb9cd8ffa5484e906274
Berezikov <iosif22ber@mail.ru>
2024-10-18 17:00:01 +0700

    Добавленны некоторые функии для меню и рандомайзер спавна врагов

--- TowerDefense/Head/Structures.h
+++ TowerDefense/Head/Structures.h
@@ -1,52 +1,56 @@
 #pragma once
 
+//Структура врага
 struct Enemy {
-	short int cost;
+	int cost;
 	short int hp;
 	short int dmg;
 	char speed;
 	char name[30];
-	char father;
-	short int child;
 	char pct;
 };
 
+//Структура босса
 struct BossEnemy {
-	short int cost;
-	short int hp;
-	char speed;
-	char pct;
+	struct Enemy boss;
 };
 
+//Структура врага, при смерти вызывающего более слабых врагов
+struct FatherEnemy {
+	struct Enemy father;
+	struct Enemy child;
+};
+
+//Структура главной башни
 struct Tower {
 	short int hp;
 	char lvl;
 };
 
+//Структура защищающей башни
 struct TowerDef {
 	short int dmg;
 	char lvl;
 	char range;
-	char speed;
+	short int speed;
 };
 
+//Структура карты
 struct Map {
 	char place[40][3];
 	char towerPlace[40];
 };
 
-struct Time {
-
-};
-
+//Структура магазина
 struct Shop {
 	int cash;
 	char towers[8];
 };
 
+//Структура раунда
 struct Round {
 	short int num;
-	short int money;
+	int money;
 	bool boss;
 	short int spawn[256];
 
--- TowerDefense/TowerDefense.cpp
+++ TowerDefense/TowerDefense.cpp
@@ -5,7 +5,9 @@
 #include <time.h>
 #include <windows.h>
 #include "head/Structures.h"
+#include <conio.h>
 
+//Начало новой игры
 void Restart(Round* round, Shop* shop, Tower* tower) {
 	(*round).num = 1;
 	(*round).money = 10;
@@ -17,6 +19,7 @@ void Restart(Round* round, Shop* shop, Tower* tower) {
 	for (int i = 0; i < 256; i++) (*round).spawn[i] = -1;
 }
 
+//Завершение раунда и подсчёт денег
 void RoundEnd(Round* round, Shop* shop, Tower* tower) {
 	(*shop).cash += (*round).num * 20;
 	(*round).num += 1;
@@ -26,13 +29,13 @@ void RoundEnd(Round* round, Shop* shop, Tower* tower) {
 	for (int i = 0; i < 256; i++) (*round).spawn[i] = -1;
 }
 
-
-
+//Покупка врага на раунд
 void EnemyBuy(Enemy enemy, Round* round, int i, int j) {
 	(*round).money = (*round).money - enemy.cost;
 	(*round).spawn[i] = j;
 }
 
+//Спавн врага
 void EnemySpawn(Round* round, Enemy enemy[], int i, Map* map) {
 	Enemy spawned;
 	spawned = enemy[(*round).spawn[i]];
@@ -40,19 +43,22 @@ void EnemySpawn(Round* round, Enemy enemy[], int i, Map* map) {
 	(*map).place[0][1] = spawned.pct;
 }
 
+//Спавн босса
 void BossSpawn(Round round, BossEnemy boss[], int i, Map* map) {
 	BossEnemy spawned;
 	spawned = boss[i];
 	//Появление босса на карте
-	(*map).place[0][1] = spawned.pct;
+	(*map).place[0][1] = spawned.boss.pct;
 }
 
-void time(Map* map) {
+//Функция времени(1 тик, за который каждый враг проходит 1 клетку)
+void timeMap(Map* map) {
 	Sleep(1000);
-	for (int i =38; i > 0; i--) (*map).place[i][1] = (*map).place[i - 1][1];
+	for (int i = 38; i > 0; i = i - 1) (*map).place[i][1] = (*map).place[i - 1][1];
 	(*map).place[0][1] = ' ';
 }
 
+//Вывод карты и измененного положения всех врагов(ещё не реализованна стрельба самих башен по врагам)
 void Changes(Map* map) {
 	system("cls");
 	for (int i = 0; i < 3; i++) {
@@ -62,6 +68,7 @@ void Changes(Map* map) {
 	for (int j = 0; j < 40; j++) printf("%c", (*map).towerPlace[j]);
 }
 
+//Покупка новой башни
 void TowerBuy(TowerDef* tower, Shop* shop, int i, Map* map) {
 	(*shop).cash = (*shop).cash - 40;
 	(*shop).towers[i] = 1;
@@ -72,26 +79,32 @@ void TowerBuy(TowerDef* tower, Shop* shop, int i, Map* map) {
 	(*map).towerPlace[5 * i + 2] = '1';
 }
 
-void TowerUpgrade(TowerDef *towers[], Shop* shop, int i, int j) {
-	(*shop).cash = (*shop).cash - (*towers[i]).lvl * 10;
-	(*towers[i]).lvl += 1;
-	if ((*towers[i]).lvl == 30) (*shop).towers[i] = -1;
-	if (j == 1) (*towers[i]).dmg += 20;
-	else if (j == 2) (*towers[i]).range += 0.25;
-	else (*towers[i]).speed += 1;
+//Прокачка уже купленной башни
+void TowerUpgrade(TowerDef *tower, Shop* shop, int i, int j) {
+	(*shop).cash = (*shop).cash - (*tower).lvl * 10;
+	(*tower).lvl += 1;
+	if ((*tower).lvl == 30) (*shop).towers[i] = -1;
+	if (j == 1) (*tower).dmg += 20;
+	else if (j == 2) (*tower).range += 0.25;
+	else (*tower).speed += 1;
 }
 
 
 int main()
 {
-	Enemy enemy[] = {{5, 100, 10, 1, "Zombe", -1, 0, 'Z'}, {2, 30, 20, 1, "Skeleton", -1, 0, 'X'}, {8, 75, 25, 2, "AngryZombe", -1, 0, '$'}};
-	BossEnemy boss[] = { {} };
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+	Enemy enemy[] = {{4, 100, 10, 1, "Zombe", 'Z'}, {2, 30, 20, 1, "Skeleton", 'X'}, {6, 75, 25, 1, "AngryZombe", 'A'}};
+	Enemy roundEnemys[] = { {} };
+	int enemyskount;
+ 	BossEnemy boss[] = { {} };
 	TowerDef towers[] = { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} };
 	Round round;
 	Shop shop;
 	Tower mainTower;
 	Map map;
 	int i;
+	//Пустая карта и начало игры
 	for (i = 0; i < 40; i++) map.place[i][0] = '-';
 	for (i = 0; i < 40; i++) map.place[i][2] = '-';
 	map.place[39][1] = '|';
@@ -100,41 +113,142 @@ int main()
 		if ((i + 3) % 5 == 0) map.towerPlace[i] = '0';
 		else map.towerPlace[i] = ' ';
 	}
-	Restart(&round, &shop, &mainTower);
-	Changes(&map);
 	short int k = 0;
-	if (round.boss == true) {
-		round.money = round.money - boss[(round.num / 10)].cost;
-		BossSpawn(round, boss, round.num/10, &map);
-		Changes(&map);
-		time(&map);
-		Changes(&map);
-		time(&map);
-		Changes(&map);
-		time(&map);
-		Changes(&map);
-	}
-	i = 0;
-	while (round.money > 1 && k < 256) {
-		//Рандомайзер случайного числа
-		EnemyBuy(enemy[i], &round, k, i);
-		i++;
-		k++;
-	}
-	for (int i = 0; i < 256; i++) {
-		if (round.spawn[i] == -1) break;
-		EnemySpawn(&round, enemy, i, &map);
-		Changes(&map);
-		time(&map);
-		Changes(&map);
-		time(&map);
-		Changes(&map);
-	}
-	TowerBuy(&towers[3], &shop, 3, &map);
-	Changes(&map);
-	do{ 
-		time(&map); 
-		Changes(&map);
-	} while (map.place[38][1] == ' ');
+	short int n = 0;
+	Restart(&round, &shop, &mainTower);
+	do {
+		printf("1 - Начать новую волну\n2 - Магазин\n3 - Выход из игры");
+		do {
+			scanf("%hd", &n);
+		} while (n < 1 || n > 3);
+		if (n == 1) {
+			//Новая волна
+			Changes(&map);
+			if (round.boss == true) {
+				round.money = round.money - boss[(round.num / 10)].boss.cost;
+				BossSpawn(round, boss, round.num / 10, &map);
+				Changes(&map);
+				timeMap(&map);
+				Changes(&map);
+				timeMap(&map);
+				Changes(&map);
+				timeMap(&map);
+				Changes(&map);
+			}
+			enemyskount = 0;
+			while (round.money > 1 && enemyskount < 256) {
+				do {
+					i = rand() % 3;
+				} while (enemy[i].cost > round.money);
+				EnemyBuy(enemy[i], &round, enemyskount, i);
+				enemyskount++;
+			}
+			i = 0;
+			while(round.spawn[i] != -1) {
+				EnemySpawn(&round, enemy, i, &map);
+				Changes(&map);
+				timeMap(&map);
+				Changes(&map);
+				timeMap(&map);
+				Changes(&map);
+				i++;
+			}
+			while (enemyskount > 0 && map.place[38][1] == ' ') {
+				timeMap(&map);
+				Changes(&map);
+			}
+		}
+		else if (n == 2) {
+			system("cls");
+			printf("Количество монет = %d\n", shop.cash);
+			printf("1 - Купить башню(40 монет)\n2 - Прокачать башню\n3 - Другое\n4 - выход");
+			do {
+				scanf("%hd", &n);
+			} while (n < 1 || n > 3);
+			
+			if (n == 1) {
+				printf("Введите номер башни(1 - 8):");
+				do {
+					scanf("%hd", &n);
+				} while (n < 1 || n > 8);
+				if (shop.cash < 40) printf("У вас недостаточно средств.");
+				else if (shop.towers[n] == 0)TowerBuy(&towers[n], &shop, n, &map);
+				else printf("Эта башня уже построена.");
+				getchar();
+			}
+			else if (n == 2) {
+				printf("Введите номер башни(1 - 8, 0 - выход):");
+				do {
+					scanf("%hd", &n);
+					if (shop.towers[n] == 0) {
+						printf("Эта башня ещё не построена.");
+						n = 0;
+					}
+				} while ((n < 0 || n > 9) && shop.towers[n] == 0);
+				if (n != 0) {
+					int j = 0;
+					if (shop.towers[n] == -1) { 
+						printf("Эта башня уже прокачана на максимум.");
+						getchar();
+						n = 0;
+					}
+					else {
+						printf("1 - Увеличить урон\n2 - Увеличить дальность\n3 - Увеличить скорость стрельбы\n0 - Выход");
+						do {
+							scanf("%d", &j);
+						} while (j < 0 || j > 3);
+					}
+					if (shop.cash < towers[n].lvl * 15) {
+						n = 0;
+						printf("У вас недостаточно средств.");
+						getchar();
+					}
+					if (j != 0 && n != 0) TowerUpgrade(&towers[n], &shop, n, j);
+				}
+			}
+			else if (n == 3) {
+				printf("1 - Восстановить здоровье главной башни(20)\n2 - Удалить башню\n3 - Выход");
+				do {
+					scanf("%hd", &n);
+				} while (n < 1 || n > 3);
+				if (n == 1) {
+					if (shop.cash < 200) { 
+						printf("У вас недостаточно средств.");
+						_getch();
+					}
+					else {
+						mainTower.hp += 500;
+						shop.cash = shop.cash - 200;
+					}
+				}
+				else if (n == 2) {
+					printf("Введите номер башни для удаления(1-8, 0 - отмена)");
+					do {
+						scanf("%hd", &n);
+					} while (n < 0 || n > 8);
+					if (n != 0) {
+						if (shop.towers[n] == 0) {
+							printf("Эта башня ещё не построена.");
+							getchar();
+						}
+						else {
+							printf("Вы уверены? Да - 1. Отмена - 0. Эта башня имеет %d уровень. Деньги за постройку и улчения назад вы не получите!", towers[n].lvl);
+							int j = 0;
+							do {
+								scanf("%d", &j);
+							} while (j < 0 || j > 1);
+							if (j == 1) {
+								shop.towers[n] = 0;
+								towers[n] = { 0, 0, 0, 0 };
+							}
+						}
+					}
+				}
+			}
+			n = 1;
+			system("cls");
+		}
+	} while (n != 3);
+	return 0;
 }
 
